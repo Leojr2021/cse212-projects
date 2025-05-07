26ed40541647248928fa552a69d859c9a6b79d0f
Mario Meza <mlj.sud.21@gmail.com>
2025-05-07 18:44:16 -0500

    Week 1 assignment

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,25 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // I will make an array to store the results
+        double[] result = new double[length];
+
+        // I will use a loop to fill the array
+        int i = 0;
+        while (i < length)
+        {
+            // I think we need to multiply the number by i + 1
+            double value = number * (i + 1);
+
+            // Put it in the array
+            result[i] = value;
+
+            // Go to next index
+            i = i + 1;
+        }
+
+        // Now return the array
+        return result;
     }
 
     /// <summary>
@@ -29,5 +47,25 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        // I think we need to save the last values first
+        List<int> lastItems = new List<int>();
+
+        int start = data.Count - amount;
+        for (int i = start; i < data.Count; i++)
+        {
+            lastItems.Add(data[i]);
+        }
+
+        // Now remove the last ones
+        for (int i = 0; i < amount; i++)
+        {
+            data.RemoveAt(data.Count - 1);
+        }
+
+        // Now add them back to the beginning
+        for (int i = 0; i < lastItems.Count; i++)
+        {
+            data.Insert(i, lastItems[i]);
+        }
     }
 }
